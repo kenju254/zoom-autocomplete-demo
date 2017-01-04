fa8c99d8f319019a0cadc7181699fdb32335669c
javathecup <github@rashid.ca>
2017-01-04 15:32:49 -0500

    editing jsonify.py and adding  pandas as a requirement

--- utils/jsonify.py
+++ utils/jsonify.py
@@ -1,16 +1,13 @@
 import os
-import csv , json
+import pandas as pd
 
 # General File and Base Directories
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = os.path.join(PROJECT_DIR, "assets/data")
 
 #Opening the Files to read and to write
-productlistCsv = open(os.path.join(DATA_DIR, 'productlist.csv'),'r')
-productlistJson = open(os.path.join(DATA_DIR, 'productlist.json'),'w')
+productlistCsv = open(os.path.join(DATA_DIR, 'productlist.csv'))
+productlistJson = open(os.path.join(DATA_DIR, 'productlist.json'))
 
-fieldNames = ("SKU", "Title", "Description")
-reader = csv.DictReader(productlistCsv, fieldNames)
-for row in reader:
-    json.dump(row, productlistJson, encoding="latin1")
-    productlistJson.write('\n')
+df = pd.read_csv('productlistCsv')
+df.to_json('productlistJson')
